2395ba0ab9cb2a1211c32a5c8b66e7e97e295bdc
jithesh <plkjithu@gmail.com>
2024-06-23 18:33:37 +0530

    requirements.txt

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jithesh/MAIN/myenv"
+set -gx VIRTUAL_ENV "/home/jithesh/Documents/main/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
